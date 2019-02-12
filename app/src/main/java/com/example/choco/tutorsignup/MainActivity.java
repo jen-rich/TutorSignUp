680d1ffea5e4b2555f738e2882cb0821bc59db62
Jen <44377149+jen-rich@users.noreply.github.com>
2019-02-12 13:59:32 -0500

    Created login screen

--- app/src/main/java/com/example/choco/tutorsignup/MainActivity.java
+++ app/src/main/java/com/example/choco/tutorsignup/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.choco.tutorsignup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+    }
+
+    public  void login() {
+        if (username.getText().toString() == "teacher" && password.getText().toString() == "tut0r!ng") {
+            String url = "http://www.scholar.google.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+
+        if (username.getText().toString() == "tutored" && password.getText().toString() == "tut0r!ng") {
+            String url = "http://www.google.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+
+        if (username.getText().toString() == "tutor" && password.getText().toString() == "tut0r!ng") {
+            String url = "http://www.google.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
     }
 }
