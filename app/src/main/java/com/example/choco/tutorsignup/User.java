74f4a1bfa5681ce3e1e41d8f1473e71822433662
haliae <bacaile35@gmail.com>
2019-02-19 13:05:44 -0500

    Added (empty) Teacher + User classes

---
+++ app/src/main/java/com/example/choco/tutorsignup/User.java
@@ -0,0 +1,9 @@
+package com.example.choco.tutorsignup;
+
+public class User {
+
+    private String firstName;
+    private String lastName; //also i'm thinking there might be a better way to store this instead of just like this so lmk if you get a better idea
+    private boolean accountType; // true -> teacher, false -> student (?) (maybe use an int if we want for additional types (sponsor, officer, etc.))
+
+}
